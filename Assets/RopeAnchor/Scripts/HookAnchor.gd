d1343095e38562cb034d950be0b4ff657ce821f2
CaioLeaoMarinho <caiolmo10@gmail.com>
2025-05-09 17:14:04 -0300

    Input feedback hook_anchor

--- Assets/RopeAnchor/Scripts/HookAnchor.gd
+++ Assets/RopeAnchor/Scripts/HookAnchor.gd
@@ -1,11 +1,14 @@
 extends Sprite2D
 
+@onready var input_icon: Sprite2D = $Cross
+
 var player = null
 var can_hook = true
-const outline_color = Color(0,0,0,1)
+const outline_color = Color(1,1,1,1)
 const outline_thickness = 2.0
 
 func _ready():
+	input_icon.visible = false
 	var selfMaterial = self.get_material().duplicate()
 	self.set_material(selfMaterial)
 	self.get_material().set_shader_parameter("line_color", outline_color)
@@ -20,6 +23,7 @@ func _on_area_2d_body_exited(body):
 		if player:
 			player = null
 			self.get_material().set_shader_parameter("enable_outline", false)
+			input_icon.visible = false
 
 func _process(_delta):
 	if player:
@@ -27,5 +31,7 @@ func _process(_delta):
 		
 		if current_player_frog_rope and current_player_frog_rope.owner == self:
 			self.get_material().set_shader_parameter("enable_outline", true)
+			input_icon.visible = true
 		else:
 			self.get_material().set_shader_parameter("enable_outline", false)
+			input_icon.visible = false
