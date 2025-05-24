b5b1119f89d72aa6a121a3d05a5a89170afda12c
ender320 <vinicius.gamed@gmail.com>
2025-05-24 12:35:54 -0300

    Cronometro + Pause Menu prontos

--- cronometro.gd
+++ cronometro.gd
@@ -8,8 +8,10 @@ func _ready():
 func _process(delta):
 	if GlobalTime.start and $Timer.is_stopped():
 		$Timer.start()
+		
 	elif not GlobalTime.start and not $Timer.is_stopped():
 		$Timer.stop()
+		$Label.modulate = Color(255,215,0)
 
 func _on_timer_timeout() -> void:
 	GlobalTime.total_time_in_secs += 1
--- reset_time_area.gd
+++ reset_time_area.gd
@@ -3,5 +3,4 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		GlobalTime.start = true
 		GlobalTime.total_time_in_secs = 0
