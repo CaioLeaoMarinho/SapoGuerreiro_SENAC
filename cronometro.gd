0a93ae972ee2c3b3b688c2ca6d58ea48269976b9
ender320 <vinicius.gamed@gmail.com>
2025-05-24 11:06:23 -0300

    cronometro funcionando

---
+++ cronometro.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	$Timer.wait_time = 1.0
+	$Timer.one_shot = false
+	$Timer.autostart = false
+
+func _process(delta):
+	if GlobalTime.start and $Timer.is_stopped():
+		$Timer.start()
+	elif not GlobalTime.start and not $Timer.is_stopped():
+		$Timer.stop()
+
+func _on_timer_timeout() -> void:
+	GlobalTime.total_time_in_secs += 1
+	var m = int(GlobalTime.total_time_in_secs / 60.0)
+	var s = GlobalTime.total_time_in_secs - m * 60
+	$Label.text = '%02d:%02d' % [m,s]
